f2e9a8b8ec6a7896d0b744ed9ae02abe13f18318
Varuna Jayasiri <vpjayasiri@gmail.com>
2020-10-14 14:20:59 +0530

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.rst", "r") as f:
 
 setuptools.setup(
     name='labml_remote',
-    version='0.0.3',
+    version='0.0.4',
     author="Varuna Jayasiri",
     author_email="vpjayasiri@gmail.com",
     description="Run python code on remote servers",
